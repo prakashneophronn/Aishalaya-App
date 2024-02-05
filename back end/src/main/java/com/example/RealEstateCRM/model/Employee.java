9a06a60585ea41082a4ce5e542a88a865037b858
Vanisri1803 <vanisriravi1118@gmail.com>
2024-02-05 12:34:26 +0530

    going to commit employee

--- back end/src/main/java/com/example/RealEstateCRM/model/Employee.java
+++ back end/src/main/java/com/example/RealEstateCRM/model/Employee.java
@@ -1,2 +1,41 @@
-package com.example.RealEstateCRM.model;public class Employee {
+package com.example.RealEstateCRM.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long employeeId;
+    private Date createdOn;
+    private String createdBy;
+    private String employeeCode;
+    private String employeeName;
+    private String prefLocation;
+    private String employeeMobile;
+    private String alterMobile;
+    private String employeeEmail;
+    private Date dob;
+    private String gender;
+    private String designation;
+    private String companyName;
+    private Long companyId;
+    private String userName;
+    private String password;
+    private String address1;
+    private String address2;
+    private String area;
+    private String city;
+    private String district;
+    private String state;
+    private String pinCode;
+    private String avatar;
+    private boolean isActive;
+
 }
+
